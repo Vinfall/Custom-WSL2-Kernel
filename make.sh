5e52ad2ecaaca58004b1f8123c8ee69c782890a8
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-03-30 14:34:14 +0800

    fix: path & pass shellcheck

--- make.sh
+++ make.sh
@@ -1,11 +1,11 @@
-#/bin/bash
+#!/bin/bash
 
 KERNEL_VER="5.15.150.1"
 
 # Get latest tarball
 wget "https://github.com/microsoft/WSL2-Linux-Kernel/archive/refs/tags/linux-msft-wsl-${KERNEL_VER}.tar.gz"
-tar xf WSL2-Linux-Kernel-linux-msft-wsl-${KERNEL_VER}.tar.gz
-cd WSL2-Linux-Kernel-linux-msft-wsl-${KERNEL_VER}
+tar xf linux-msft-wsl-${KERNEL_VER}.tar.gz --directory=linux-msft-wsl-${KERNEL_VER}
+cd linux-msft-wsl-${KERNEL_VER} || exit
 unset KERNEL_VER
 
 # Patch
@@ -15,9 +15,9 @@ patch .config < dm-crypt-plus-usb.patch
 # Compile kernel
 make
 mkdir output
-cp arch/x86/boot/bzImage output/$(date +'%Y%m%d')-bzImage
+cp arch/x86/boot/bzImage output/"$(date +'%Y%m%d')"-bzImage
 # Compile USBIP
-cd tools/usb/usbip
+cd tools/usb/usbip || exit
 ./autogen.sh
 ./configure
 make install
