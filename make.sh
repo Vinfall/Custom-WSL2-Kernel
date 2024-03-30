a4a23a17c611adcc6390656e93302530100a2909
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-03-30 14:28:55 +0800

    feat: add build script

---
+++ make.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+KERNEL_VER="5.15.150.1"
+
+# Get latest tarball
+wget "https://github.com/microsoft/WSL2-Linux-Kernel/archive/refs/tags/linux-msft-wsl-${KERNEL_VER}.tar.gz"
+tar xf WSL2-Linux-Kernel-linux-msft-wsl-${KERNEL_VER}.tar.gz
+cd WSL2-Linux-Kernel-linux-msft-wsl-${KERNEL_VER}
+unset KERNEL_VER
+
+# Patch
+cp Microsoft/config-wsl .config
+patch .config < dm-crypt-plus-usb.patch
+
+# Compile kernel
+make
+mkdir output
+cp arch/x86/boot/bzImage output/$(date +'%Y%m%d')-bzImage
+# Compile USBIP
+cd tools/usb/usbip
+./autogen.sh
+./configure
+make install
+cp libsrc/.libs/libusbip.so.0 output/libusbip.so.0
+
